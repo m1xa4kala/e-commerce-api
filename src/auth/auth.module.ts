703994ec1e0dbbdb267908100e70e229372d6046
m1xa4kala <maikl2020011@gmail.com>
2024-06-09 02:23:36 +0300

    add jwt guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,10 +6,11 @@ import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from 'src/users/users.module';
 import { options } from './config';
 import { STRATEGIES } from './strategies';
+import { GUARDS } from './guards';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, ...STRATEGIES],
+  providers: [AuthService, ...STRATEGIES, ...GUARDS],
   imports: [PassportModule, JwtModule.registerAsync(options()), UsersModule],
   exports: [AuthService],
 })
